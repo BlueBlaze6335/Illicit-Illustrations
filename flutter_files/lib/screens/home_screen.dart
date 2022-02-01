1c633d90727f7263c0c6ea2f1996a2f2649a0fa3
Sumi4482 <sbanwakde4482@gmail.com>
2022-02-01 14:50:42 +0530

    Added Glow Button

--- flutter_files/lib/screens/home_screen.dart
+++ flutter_files/lib/screens/home_screen.dart
@@ -1,13 +1,9 @@
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:illicit_illustrations_2/screens/image_editor_screen.dart';
 
 import 'package:image_picker/image_picker.dart';
-
-
-
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -17,7 +13,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   ///variable to store [Xfile?] returned by image picker package
   dynamic image;
 
@@ -45,7 +40,6 @@ class _HomeScreenState extends State<HomeScreen> {
 
   ///this function to grab the image from gallery and pass it onto the image editor screen to be processed
   pickGalleryImage() async {
-
     image = await picker.pickImage(source: ImageSource.gallery);
     if (image == null) return null;
 
@@ -58,74 +52,86 @@ class _HomeScreenState extends State<HomeScreen> {
         ));
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
         body: Stack(
-          children: [
+      children: [
+        /// Image asset for background image
+        Image.asset(
+          'assets/home_screen_bg.png',
+          width: double.infinity,
+          height: double.infinity,
+          fit: BoxFit.fill,
+        ),
 
-            /// Image asset for background image
-            Image.asset('assets/home_screen_bg.png',width: double.infinity,height: double.infinity, fit: BoxFit.fill,),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(80, 40, 80, 60),
 
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(80,40,80,60),
-
-                /// Row widget containing buttons for picking image from camera/gallery
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-
-                    ///Pick image from camera button
-                    InkWell(
-                      onTap: () => pickImage(),
-                      child: Container(
-                        height: 60,
-                        width: 60,
-                        decoration: const BoxDecoration(
-                            color: Color(0xFF57CACE),
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(20),
-                              bottomLeft: Radius.circular(20),
-                            )
-                        ),
-
-                        child: Image.asset('assets/shutter_icon.png',),
+            /// Row widget containing buttons for picking image from camera/gallery
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ///Pick image from camera button
+                InkWell(
+                  onTap: () => pickImage(),
+                  child: Container(
+                    height: 60,
+                    width: 60,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF57CACE),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomLeft: Radius.circular(20),
                       ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(52, 231, 237, 1),
+                          blurRadius: 7.0,
+                          spreadRadius: 2.0,
+                          offset: Offset(-4.0, 0.0),
+                        ),
+                      ],
                     ),
-
-
-                    ///Pick image from gallery button
-                    InkWell(
-                      onTap: () => pickGalleryImage(),
-                      child:  Container(
-                        height: 60,
-                        width: 60,
-                        decoration: const BoxDecoration(
-                            color: Color(0xFFD27AE7),
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(20),
-                              bottomRight: Radius.circular(20),
-                            )
-                        ),
-
-                        child: Image.asset('assets/gallery_icon_2.png',),
-                      ),
-
-                    )
-
-
-
-                  ],
+                    child: Image.asset(
+                      'assets/shutter_icon.png',
+                    ),
+                  ),
                 ),
-              ),
-            )
 
-          ],
+                ///Pick image from gallery button
+                InkWell(
+                  onTap: () => pickGalleryImage(),
+                  child: Container(
+                    height: 60,
+                    width: 60,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFD27AE7),
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(242, 88, 252, 1),
+                          blurRadius: 8.0,
+                          spreadRadius: 2.0,
+                          offset: Offset(2.0, -1.0),
+                        ),
+                      ],
+                    ),
+                    child: Image.asset(
+                      'assets/gallery_icon_2.png',
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         )
-    );
+      ],
+    ));
   }
 }
