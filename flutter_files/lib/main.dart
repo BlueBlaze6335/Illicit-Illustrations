9b6ca5c1f93b3801daf8ec94ac2728197e6295ab
XxThunderBlastxX <koustavmondal55@gmail.com>
2022-02-01 22:11:28 +0530

    added Splash screen

--- flutter_files/lib/main.dart
+++ flutter_files/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:illicit_illustrations_2/screens/splash_screen.dart';
+import 'package:illicit_illustrations_2/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SplashScreen(),
+      home: HomeScreen(),
     );
   }
 }
-
